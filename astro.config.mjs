5676825f087e3adb4a5ed59b566bb5a9fdae7953
Riverxia <riverdalecoder@outlook.com>
2026-01-11 21:33:57 -0500

    Fixed tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  site: 'static',
+  integrations: [tailwind(), mdx(), sitemap()],
   adapter: vercel(),
 });
