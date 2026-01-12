a2fbf626d3845cdfcd33ba1e7d4e06ef8b560d40
Riverxia <riverdalecoder@outlook.com>
2026-01-11 21:35:57 -0500

    Tailwind import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
+import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
