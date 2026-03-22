6a4df019e487169c251a75b4b4e7d5d2350aefe2
Riverxia <riverdalecoder@outlook.com>
2026-03-22 13:36:30 -0400

    (acm) Currently Listening To: Heart Attack (Rock Version) - Demi Lovato

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'idktheflag docs',
-      customCss: [
-        // We will add your Neovim/Terminal aesthetic CSS here later
-      ],
-      sidebar: [
-        {
-          label: 'CTF Writeups',
-          autogenerate: { directory: 'writeups' },
-        },
-        {
-          label: 'Infrastructure',
-          autogenerate: { directory: 'infra' },
-        }
-      ],
-    }),
-  ],
+  integrations: [],
 });
